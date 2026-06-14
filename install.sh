d44c4c8aef6d482a5cfceb1e056f3b4c17603aa9
JudicaelT <judicael@lesterrisse.com>
2026-06-15 01:27:57 +0200

    updated install script to move config files

--- install.sh
+++ install.sh
@@ -6,8 +6,15 @@ BOLD_YELLOW="\e[1;33m"
 BOLD_GREEN="\e[1;32m"
 BOLD_RED="\e[1;31m"
 NC="\e[0m"
+CONFIG_DIR="~/.config"
+SOURCE_CONFIG_DIR="$(pwd)/.config"
 
-packages=(
+if [ ! -d "$SOURCE_CONFIG_DIR" ]; then
+    echo -e "${BOLD_RED}Could not find ${SOURCE_CONFIG_DIR}. Exiting...${NC}"
+    return 1
+fi
+
+PACKAGES=(
     aylurs-gtk-shell
     htop
     hyprland
@@ -36,8 +43,8 @@ echo "║                                                        ║"
 echo "╠════════════════════════════════════════════════════════╣"
 echo "║  The following packages will be installed:             ║"
 
-for package in "${packages[@]}"; do
-    printf "║   - %-50s║\n" "$package"
+for package in "${PACKAGES[@]}"; do
+    printf "║   - %-50s ║\n" "$package"
 done
 
 echo "╚════════════════════════════════════════════════════════╝"
@@ -54,7 +61,7 @@ case "$confirm" in [yY]|[yY][eE][sS])
         ;;
 esac
 
-for package in "${packages[@]}"; do
+for package in "${PACKAGES[@]}"; do
     echo -e "${BOLD_YELLOW}Installing ${package}...${NC}"
 
     if yay -S --noconfirm --needed "$package"; then
@@ -63,6 +70,32 @@ for package in "${packages[@]}"; do
         echo -e "${BOLD_RED}✗ Failed to install ${package}${NC}"
         exit 1
     fi
+done
+
+echo -e "${BOLD_YELLOW}Setting up ~/.config folder...${NC}"
+
+if [ ! -d "$CONFIG_DIR/ags" ]; then
+    echo -e "${BOLD_YELLOW}Initializing AGS...${NC}"
+    ags init
+fi
+echo -e "${BOLD_YELLOW}Cleaning AGS directory...${NC}"
+find "$CONFIG_DIR/ags" -mindepth 1 \
+    ! -name "app.ts" \
+    ! -name "env.d.ts" \
+    ! -name ".gitignore" \
+    ! -name "package.json" \
+    ! -name "style.css" \
+    ! -name "tsconfig.json" \
+    ! -name "widget/" \
+    -exec rm -rf {} +
+
+echo -e "${BOLD_YELLOW}Updating AGS configuration...${NC}"
+cp -rf "$SOURCE_CONFIG_DIR/ags/"* "$CONFIG_DIR/ags/"
+
+echo -e "${BOLD_YELLOW}Copying configuration files...${NC}"
+for config in "$SOURCE_CONFIG_DIR"/*; do
+    [ "$(basename "$config")" = "ags" ] && continue
+    cp -rf "$config" "$CONFIG_DIR/"
 done
 
 echo
