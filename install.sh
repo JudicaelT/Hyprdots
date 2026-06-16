83415f1232ce624234992efbe0fcd9b661f9b36f
JudicaelT <judicael@lesterrisse.com>
2026-06-16 18:20:26 +0200

    refactor install script + install starship from AUR

--- install.sh
+++ install.sh
@@ -8,12 +8,6 @@ BOLD_RED="\e[1;31m"
 NC="\e[0m"
 CONFIG_DIR=~/.config
 SOURCE_CONFIG_DIR="$(pwd)/.config"
-
-if ! test -d $SOURCE_CONFIG_DIR; then
-    echo -e "${BOLD_RED}Could not find ${SOURCE_CONFIG_DIR}. Exiting...${NC}"
-    return 1
-fi
-
 PACKAGES=(
     aylurs-gtk-shell
     htop
@@ -31,71 +25,79 @@ PACKAGES=(
     nodejs
     npm
     ripgrep
+    starship
     wofi
     zoxide
 )
 
-echo -e "${BOLD_YELLOW}"
-echo "╔════════════════════════════════════════════════════════╗"
-echo "║                                                        ║"
-echo "║                  Hyprdots installer                    ║"
-echo "║                                                        ║"
-echo "╠════════════════════════════════════════════════════════╣"
-echo "║  The following packages will be installed:             ║"
-
-for package in "${PACKAGES[@]}"; do
-    printf "║   - %-50s ║\n" "$package"
-done
-
-echo "╚════════════════════════════════════════════════════════╝"
-echo -e "${NC}"
-
-read -rp "Continue? [y/N]: " confirm
-
-case "$confirm" in [yY]|[yY][eE][sS])
-        echo -e "${BOLD_GREEN}Starting installation...${NC}"
-        ;;
-    *)
-        echo -e "${BOLD_RED}Installation cancelled.${NC}"
-        exit 0
-        ;;
-esac
-
-for package in "${PACKAGES[@]}"; do
-    echo -e "${BOLD_YELLOW}Installing ${package}...${NC}"
-
-    if yay -S --noconfirm --needed "$package"; then
-        echo -e "${BOLD_GREEN}✓ ${package} installed${NC}"
-    else
-        echo -e "${BOLD_RED}✗ Failed to install ${package}${NC}"
-        exit 1
-    fi
-done
-
-echo -e "${BOLD_YELLOW}Setting up ~/.config folder...${NC}"
-
-if ! test -d $CONFIG_DIR/ags; then
-    echo -e "${BOLD_YELLOW}Initializing AGS...${NC}"
-    ags init
+if ! test -d $SOURCE_CONFIG_DIR; then
+    echo -e "${BOLD_RED}Could not find ${SOURCE_CONFIG_DIR}. Exiting...${NC}"
+    exit 1
 fi
 
-echo -e "${BOLD_YELLOW}Cleaning AGS directory...${NC}"
-rm -rf $CONFIG_DIR/ags/app.ts
-rm -rf $CONFIG_DIR/ags/env.d.ts
-rm -rf $CONFIG_DIR/ags/.gitignore
-rm -rf $CONFIG_DIR/ags/package.json
-rm -rf $CONFIG_DIR/ags/style.css
-rm -rf $CONFIG_DIR/ags/tsconfig.json
-rm -rf $CONFIG_DIR/ags/widget/
+print_recap() {
+    echo -e "${BOLD_YELLOW}"
+    echo "╔════════════════════════════════════════════════════════╗"
+    echo "║                                                        ║"
+    echo "║                Arch Hyprdots installer                 ║"
+    echo "║                                                        ║"
+    echo "╠════════════════════════════════════════════════════════╣"
+    echo "║  The following packages will be installed:             ║"
 
-echo -e "${BOLD_YELLOW}Updating AGS configuration...${NC}"
-cp -rf "$SOURCE_CONFIG_DIR/ags/"* "$CONFIG_DIR/ags/"
+    for package in "${PACKAGES[@]}"; do
+        printf "║   - %-50s ║\n" "$package"
+    done
 
-echo -e "${BOLD_YELLOW}Copying configuration files...${NC}"
-for config in "$SOURCE_CONFIG_DIR"/*; do
-    [ "$(basename "$config")" = "ags" ] && continue
-    cp -rf "$config" "$CONFIG_DIR/"
-done
+    echo "╚════════════════════════════════════════════════════════╝"
+    echo -e "${NC}"
 
-echo
-echo -e "${BOLD_GREEN}All done! You may now restart your computer.${NC}"
+    read -rp "Continue? [y/N]: " confirm
+    if "$confirm" in [yY]|[yY][eE][sS]; then
+        echo -e "${BOLD_GREEN}Starting installation...${NC}";
+        echo
+    else
+        echo -e "${BOLD_RED}Installation cancelled.${NC}";
+        exit 0;
+    fi
+}
+print_recap()
+
+install_packages() {
+    for package in "${PACKAGES[@]}"; do
+        echo -e "${BOLD_YELLOW}Installing ${package}...${NC}"
+
+        if yay -S --noconfirm --needed "$package"; then
+            echo -e "${BOLD_GREEN}✓ ${package} installed${NC}"
+        else
+            echo -e "${BOLD_RED}✗ Failed to install ${package}${NC}"
+            exit 1
+        fi
+    done
+}
+install_packages()
+
+setup_config_dir() {
+    if ! test -d $CONFIG_DIR/ags; then
+        echo -e "${BOLD_YELLOW}Initializing AGS...${NC}"
+        ags init
+        echo
+    fi
+    echo -e "${BOLD_YELLOW}Copying configuration files...${NC}"
+    rm -rf $CONFIG_DIR/ags/app.ts
+    rm -rf $CONFIG_DIR/ags/env.d.ts
+    rm -rf $CONFIG_DIR/ags/.gitignore
+    rm -rf $CONFIG_DIR/ags/package.json
+    rm -rf $CONFIG_DIR/ags/style.css
+    rm -rf $CONFIG_DIR/ags/tsconfig.json
+    rm -rf $CONFIG_DIR/ags/widget/
+    cp -rf "$SOURCE_CONFIG_DIR/ags/"* "$CONFIG_DIR/ags/"
+    for config in "$SOURCE_CONFIG_DIR"/*; do
+        # We do not override the entire ags folder because it contains auto-generated code.
+        if "$(basename "$config")" != "ags"; then
+            cp -rf "$config" "$CONFIG_DIR/"
+        fi
+    done
+}
+setup_config_dir()
+
+echo -e "\n${BOLD_GREEN}All done! You may now restart your computer.${NC}"
